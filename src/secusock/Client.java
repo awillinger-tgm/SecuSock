8b44b28336acf59fee42f3ea178449a1dbf34f37
Andreas Willinger <awi95@gmx.at>
2015-01-29 14:35:55 +0100

    Implemented Client/server, removed Start class

--- src/secusock/Client.java
+++ src/secusock/Client.java
@@ -1,6 +1,181 @@
 package secusock;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.security.KeyPair;
+
+import javax.crypto.SecretKey;
+
+import secusock.cipher.SessionKey;
+import secusock.cipher.SharingKey;
+
+/**
+ * This class implements the SecuSock client, which takes user input and sends
+ * it to the server.
+ * 
+ * @author Andreas Willinger
+ * @version 20150129.1
+ */
 public class Client
 {
+	private String host = "";
+	private int port = -1;
+	// SharingKey
+	private KeyPair keyPair;
+	// SessionKey
+	private SecretKey sessionKey;
 
+	public Client(String host, int port)
+	{
+		// Sockets don't really like "localhost" addresses
+		this.host = (host.equals("localhost")) ? "127.0.0.1" : host;
+		this.port = port;
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			// static context, ugh
+			Client client = new Client(args[0], Integer.parseInt(args[1]));
+			System.out
+					.println("Welcome to SecuSock!\nThis application allows you to send plain-text and encrypted message to a server.");
+			System.out
+					.println("\nAvailable commands are:\n\t!keys - generate a new SessioNKey and enable encryption.\n\t_quit - exit the application\n");
+			client.run();
+		}
+		catch (ArrayIndexOutOfBoundsException | NumberFormatException ex)
+		{
+			System.err.println("Missing parameters or invalid parameter types!");
+			System.out.println("Usage:\njava Client <host> <port>\nExample: java Client 127.0.0.1 1234");
+			System.out.println("\t<host> The IP of the host the server is running on.");
+			System.out.println("\t<port> The port the server is running on.");
+		}
+	}
+
+	/**
+	 * Opens a socket connection to the server and reads user input. Then either
+	 * requests a SessionKey from the server or sends the message (encrypted) to
+	 * the server.
+	 */
+	public void run()
+	{
+		try (BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+				Socket socket = new Socket(this.host, this.port);
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));)
+		{
+			while (true)
+			{
+				System.out.print("Command> ");
+
+				String fromUser = stdIn.readLine();
+				String fromServer;
+
+				if (fromUser != null)
+				{
+					// sanitize string to check for commands
+					// remove whitespace + make all characters lower case
+					String sanitized = fromUser.trim().toLowerCase();
+
+					// generate a new SharingKey and request
+					if (sanitized.equals("!keys"))
+					{
+						// locally save the RSA KeyPair, for decrypting the
+						// sessionkey we get from the server
+						this.keyPair = SharingKey.generateKeyPair();
+
+						// send public key to server
+						out.println("GENKEY|" + SharingKey.encodePublicKey(this.keyPair));
+						out.flush();
+
+						System.out.println("Sent request to server to generate a new SessionKey.");
+					}
+					else if (sanitized.equals("_quit"))
+					{
+						System.out.println("Closing sockets and shutting down.");
+
+						break;
+					}
+					// no command entered
+					// check if a SessionKey was set, and if yes, send a
+					// encrypted message
+					else if (this.sessionKey != null)
+					{
+
+						String encrypted = SessionKey.encrypt(this.sessionKey, fromUser);
+						System.out.println("Sending encrypted message: " + encrypted);
+
+						out.println("ENCMSG|" + encrypted);
+						out.flush();
+					}
+					// send message as plain-text
+					else
+					{
+						System.out.println("Sending plain-text message: " + fromUser);
+
+						out.println("PLAINMSG|" + fromUser);
+						out.flush();
+
+					}
+
+					// read reply from server
+					while ((fromServer = in.readLine()) != null)
+					{
+						this.handleServerMessage(fromServer);
+						break;
+					}
+				}
+				else
+				{
+					System.err.println("Invalid command specified!");
+					break;
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			System.err.println("Fatal error: " + ex.getMessage());
+		}
+	}
+
+	/**
+	 * Handles messages received from the server.
+	 * 
+	 * @param message
+	 *            The message which was received from the server, as a String.
+	 * @throws Exception
+	 */
+	private void handleServerMessage(String message) throws Exception
+	{
+		if (message == null || message.length() == 0)
+		{
+			System.err.println("Invalid, empty message from server.");
+			return;
+		}
+
+		// messages have the format COMMAND|PAYLOAD
+		String command = message.substring(0, message.indexOf("|"));
+		String payload = message.substring(message.indexOf("|") + 1, message.length());
+
+		if (command.equals("SETKEY"))
+		{
+			// decrypt the received sessionkey using our private key
+			byte[] decrypted = SharingKey.decrypt(this.keyPair.getPrivate(), payload);
+			this.sessionKey = SessionKey.getKey(decrypted);
+
+			System.out.println("Got a new SessionKey from server.");
+			System.out.println("All messages are now being transmitted AES 256 bit encrypted!");
+		}
+		else if (command.equals("GOTMESSAGE"))
+		{
+			System.out.println("Server got message.");
+		}
+		else
+		{
+			System.err.println("Got an unknown command from server: " + command);
+		}
+	}
 }
--- src/secusock/Server.java
+++ src/secusock/Server.java
@@ -1,31 +1,128 @@
 package secusock;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server implements Runnable
+import javax.crypto.SecretKey;
+
+import secusock.cipher.SessionKey;
+import secusock.cipher.SharingKey;
+
+/**
+ * This class implements the SecuSock server, which handles SessioNKey
+ * generation and replies to the client.
+ * 
+ * @author Andreas Willinger
+ * @version 20150129.1
+ */
+public class Server
 {
-	private PrintWriter out = null;
-	private BufferedReader in = null;
-	
-	public void run()
+	private int port = -1;
+
+	// SessioNkey
+	private SecretKey sessionKey;
+
+	public Server(int port)
+	{
+		this.port = port;
+	}
+
+	public static void main(String[] args) throws Exception
 	{
 		try
 		{
-			ServerSocket serverSocket = new ServerSocket(42069);
-			Socket remoteSocket = serverSocket.accept();
-			
-			this.out = new PrintWriter(remoteSocket.getOutputStream(),
-					true);
-			this.in = new BufferedReader(new InputStreamReader(
-					remoteSocket.getInputStream()));
+			// static context, ugh
+			Server server = new Server(Integer.parseInt(args[0]));
+			System.out.println("Starting SecuSock server ..");
+
+			server.run();
 		}
-		catch (IOException e)
+		catch (ArrayIndexOutOfBoundsException | NumberFormatException ex)
 		{
+			System.err.println("Missing parameters or invalid parameter types!");
+			System.out.println("Usage:\njava Server <port>\nExample: java Server 1234");
+			System.out.println("\t<port> The port the server is running on.");
+		}
+	}
+
+	/**
+	 * Opens a server socket and waits for connections and handles messages once
+	 * they arrive.
+	 * 
+	 * @throws Exception
+	 */
+	public void run()
+	{
+		System.out.println("SecuSock server started, waiting for connections ..");
+
+		while (true)
+		{
+			try (ServerSocket serverSocket = new ServerSocket(this.port);
+					Socket clientSocket = serverSocket.accept();
+					PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+					BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));)
+			{
+				System.out.println("Connection accepted from " + clientSocket.getInetAddress() + ":"
+						+ clientSocket.getPort());
+
+				String inputLine = "";
+
+				while ((inputLine = in.readLine()) != null)
+				{
+					// messages have the format COMMAND|PAYLOAD
+					String command = inputLine.substring(0, inputLine.indexOf("|"));
+					String payload = inputLine.substring(inputLine.indexOf("|") + 1, inputLine.length());
+
+					// Client sent a public key and requests a common SessionKey
+					// Generate one, and send the SessionKey back encrypted.
+					if (command.equals("GENKEY"))
+					{
+						this.sessionKey = SessionKey.generateKey();
+
+						System.out.println("\nReceived SessionKey generation request");
+						System.out.println("--------------------------------------");
+						System.out.println("Public key: " + payload);
+
+						String cipherText = SharingKey.encrypt(SharingKey.decodePublicKey(payload),
+								this.sessionKey.getEncoded());
+
+						out.println("SETKEY|" + cipherText);
+					}
+					// Client sent a encrypted message
+					else if (command.equals("ENCMSG"))
+					{
+						String deciphered = SessionKey.decrypt(this.sessionKey, payload);
+
+						System.out.println("\nReceived encrypted message");
+						System.out.println("--------------------------");
+						System.out.println("Encrypted: " + payload + "\nDecrypted: " + deciphered);
+
+						out.println("GOTMESSAGE|0");
+					}
+					// Client sent plain-text message
+					else if (command.equals("PLAINMSG"))
+					{
+						System.out.println("\nReceived plain-text message");
+						System.out.println("---------------------------");
+						System.out.println("Message: " + payload);
+						out.println("GOTMESSAGE|0");
+					}
+					// Client sent something unknown
+					else
+					{
+						out.println("INVALID|Unknown command");
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				System.err.println("Fatal error: " + ex.getMessage());
+				break;
+			}
+			System.out.println("Connection closed.");
 		}
 	}
 }
